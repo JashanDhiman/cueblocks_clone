018250e5254e26ec6d648b82e8f9c4fc5d05e97f
jashan <jashandhiman.hulk@gmail.com>
2022-03-23 13:02:58 +0530

    testing something

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,19 @@
 import { Button } from "@mui/material";
 import React from "react";
+import image from "./logo192.png";
 
 const MainPage = () => {
   return (
     <>
+      <h1>my heading</h1>
+      <div>Blog</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <img src={image} alt=""></img>
       <a href="/contact">
         <Button>Contact Page</Button>
       </a>

587f5fbdb1c383ca833c182dfe998cc29b8348ec
jashan <jashandhiman.hulk@gmail.com>
2022-03-23 15:39:43 +0530

    tailwind css trial2

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,13 +7,186 @@ const MainPage = () => {
     <>
       <h1>my heading</h1>
       <div>Blog</div>
+      <p className="text-xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-1xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-4xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-5xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-6xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-7xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-8xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-9xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-1xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-2xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-3xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-4xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-5xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-6xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-7xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-8xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-9xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
       <p className="text-xl font-bold underline">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
         quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
         voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
         asperiores!
       </p>
-      <img src={image} alt=""></img>
+      <p className="text-1xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-2xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-3xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-4xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-5xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-6xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-7xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-8xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
+      <p className="text-9xl font-bold underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus
+        quod fugit in dolores aliquid error vero quas, ipsam placeat repudiandae
+        voluptatem cupiditate doloribus ipsa nesciunt deserunt reiciendis omnis
+        asperiores!
+      </p>
       <a href="/contact">
         <Button>Contact Page</Button>
       </a>
